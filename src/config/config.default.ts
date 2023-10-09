93da98c9e6f4efdd5244ce57a10b213bbc8eed3c
Nihao Saoa <nihaosaoa@nihaosaoa.local>
2023-10-08 23:54:53 -0600

    add postgres data

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,4 +1,5 @@
 import { MidwayConfig } from '@midwayjs/core';
+import { Photo } from '../entity/photo.entity';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
@@ -8,5 +9,25 @@ export default {
   },
   view: {
     defaultViewEngine: 'nunjucks',
+  },
+  typeorm: {
+    dataSource: {
+      default: {
+        /**
+         * 单数据库实例
+         */
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '321ewqdsacxz',
+        database: 'test',
+        synchronize: false,     // 如果第一次使用，不存在表，有同步的需求可以写 true，注意会丢数据
+        logging: false,
+
+        // 配置实体模型
+        entities: [Photo],
+      }
+    }
   },
 } as MidwayConfig;
